4445183d3d2bce8f7e603f8fc7233f39596b04c1
jagrivero <jarivero@frba.utn.edu.ar>
2025-06-20 10:59:56 -0300

    Nota: Se llama Aplicacion y no WebApp. Cambio el lugar

--- src/main/java/ar/edu/utn/dds/k3003/Application.java
+++ src/main/java/ar/edu/utn/dds/k3003/Application.java
@@ -1,4 +1,4 @@
-package ar.edu.utn.dds.k3003.app;
+package ar.edu.utn.dds.k3003;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
